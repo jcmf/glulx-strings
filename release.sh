b124c69b3966745b218c52ddde8b7b793808f2b1
Jacques Frechet <zaphod-git@toastball.net>
2015-02-16 14:41:24 -0800

    fix release script

--- release.sh
+++ release.sh
@@ -9,8 +9,7 @@ npm version --git-tag-version=false "$@"
 version="v`node -e 'console.log(JSON.parse(require("fs").readFileSync("package.json", "utf8")).version)'`"
 npm install
 git commit -am "$version"
-git tag -am "$version"
+git tag -am "$version" "$version"
 git push
 npm publish
-ssh toastball.net 'cd www/toastball.net/glulx-strings && git pull`
-
+ssh toastball.net 'cd www/toastball.net/glulx-strings && git pull'
