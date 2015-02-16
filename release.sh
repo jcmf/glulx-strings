d31546f0318739ed157d58bf88b6c9c5477f3a7e
Jacques Frechet <zaphod-git@toastball.net>
2015-02-16 14:45:25 -0800

    only build twice

--- release.sh
+++ release.sh
@@ -7,9 +7,8 @@ npm install
 test -z "`git status --porcelain 2>&1`"
 npm version --git-tag-version=false "$@"
 version="v`node -e 'console.log(JSON.parse(require("fs").readFileSync("package.json", "utf8")).version)'`"
-npm install
+npm publish
 git commit -am "$version"
 git tag -am "$version" "$version"
 git push
-npm publish
 ssh toastball.net 'cd www/toastball.net/glulx-strings && git pull'
