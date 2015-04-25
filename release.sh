2bf02a018a7f8fc0f5f56d7f2a4a142fc31669ef
Jacques Frechet <zaphod-git@toastball.net>
2015-04-24 20:48:12 -0700

    got worried that npm publish might be packing up some git state, so maybe do git first?

--- release.sh
+++ release.sh
@@ -7,8 +7,9 @@ npm install
 test -z "`git status --porcelain 2>&1`"
 npm version --git-tag-version=false "$@"
 version="v`node -e 'console.log(JSON.parse(require("fs").readFileSync("package.json", "utf8")).version)'`"
-npm publish
+npm install
 git commit -am "$version"
 git tag -am "$version" "$version"
 git push
+npm publish
 ssh toastball.net 'cd www/toastball.net/glulx-strings && git pull'
